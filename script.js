72c98defb1dc7b6fb559bc7d62f3cd1438160d11
Mapita09 <mapalaiton@gmail.com>
2025-09-03 22:54:27 +0000

    Have you ever tried THIS ONEEEE

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 console.log ("Que más wey!");
 
-const titulo = document. getElementById ("PrincialTitulo")
+const titulo = document.getElementById ("PrincipalTitulo");
+let estadoOriginal = true;
+
+console.log(titulo);
+
+titulo.innerText = "Have you ever tried this oneeee?";
+
+titulo.addEventListener("click", () => {
+    if(estadoOriginal === true) {
+        titulo.innerText = "Dance break";
+        titulo.style.color = "light pink";
+    }
+
+
+else { 
+    titulo.innerText = "Titulo Bonito";
+    titulo.style.color = "white";
+}
+     })
+    
